d424e780d0252a4f0e1d8b57970612c11870f28a
Mbefou Delsong <delsongleong@gmail.com>
2026-06-17 10:10:12 +0100

    implement fonts

--- my-app/app/(tabs)/index.tsx
+++ my-app/app/(tabs)/index.tsx
@@ -9,29 +9,29 @@ const SafeAreaView = styled(RNSafeAreaView)
 export default function App() {
     return (
         <SafeAreaView className="flex-1  bg-background p-5">
-            <Text className="text-xl font-bold text-success">
-                Welcome to Nativewind!
-            </Text>
-            <Link href="/onboarding" className="mt-4 rounded bg-primary text-white p-4">
+            <Text className="text-7xl font-sans-extrabold text-primary">Home</Text>
+
+            <Link href="/onboarding" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
                 Go to Onboarding
             </Link>
-            <Link href="/(auth)/sign-in" className="mt-4 rounded bg-primary text-white p-4">
+            <Link href="/(auth)/sign-in" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
                 Go to SignIn
             </Link>
-            <Link href="/(auth)/sign-up" className="mt-4 rounded bg-primary text-white p-4">
+            <Link href="/(auth)/sign-up" className="mt-4 font-sans-bold rounded bg-primary text-white p-4">
                 Go to SignUp
             </Link>
-            <Link href="/subscriptions/spotify">
-                Spotify Subscription.
-            </Link>
-            <Link
-                href={{
-                    pathname: "/subscriptions/[id]",
-                    params: { id: "claude" },
-            }}
-            >
-                Claude Max Subscription
-            </Link>
+
+            {/*<Link href="/subscriptions/spotify">*/}
+            {/*    Spotify Subscription.*/}
+            {/*</Link>*/}
+            {/*<Link*/}
+            {/*    href={{*/}
+            {/*        pathname: "/subscriptions/[id]",*/}
+            {/*        params: { id: "claude" },*/}
+            {/*}}*/}
+            {/*>*/}
+            {/*    Claude Max Subscription*/}
+            {/*</Link>*/}
         </SafeAreaView>
     );
 }
